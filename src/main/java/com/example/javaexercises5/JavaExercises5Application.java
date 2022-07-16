3c1226e1eb43cf631dabf0d8e7fb72e4bdc00cb1
simo333 <szymon.333@wp.pl>
2022-07-16 14:31:08 +0200

    Initial commit

---
+++ src/main/java/com/example/javaexercises5/JavaExercises5Application.java
@@ -0,0 +1,13 @@
+package com.example.javaexercises5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaExercises5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaExercises5Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javaexercises5/JavaExercises5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaexercises5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaExercises5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
