4a5b9be097fb177c4bc160b1689c262c9aebffab
simo333 <szymon.333@wp.pl>
2022-07-21 23:08:05 +0200

    Extra tasks #2.6

--- src/main/java/com/example/javaexercises5/advanced/java8/Task06/Main06.java
+++ src/main/java/com/example/javaexercises5/advanced/java8/Task06/Main06.java
@@ -4,20 +4,22 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.stream.Stream;
 
 public class Main06 {
     public static void main(String[] args) {
         Path path = Paths.get("earnings.txt");
-        try {
-            List<String> strings = Files.readAllLines(path);
-            strings.stream()
-                    .filter(s -> s.contains(";"))
-                    .filter(s -> s.matches("[^Kowalsk]"))
-                    .sorted()
+        try (Stream<String> lines = Files.lines(path)) {
+            lines
+                    .filter(e -> !e.matches(".*Kowalsk.*"))
+                    .map(e -> e.split(",|;"))
+                    .filter(e -> e.length > 2)
+                    .filter(e -> e[2].length() > 1)
+                    .peek(e -> e[2] = e[2].substring(0, e[2].length() - 3))
+                    .sorted((e1, e2) -> Double.compare(Double.parseDouble(e2[2]), Double.parseDouble(e1[2])))
                     .limit(3)
-                    .forEach(System.out::println);
+                    .forEach(e -> System.out.println(e[0] + " " + e[1] + " -" + e[2]));
+
         } catch (IOException e) {
             System.out.println("Cannot read file.");
         }
