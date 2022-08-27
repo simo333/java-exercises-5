6f68680e516442f0d3ecb2dac6b0312271b22d1d
simo333 <szymon.333@wp.pl>
2022-08-27 19:16:52 +0200

    Dates

--- src/main/java/com/example/javaexercises5/dateapi/Task06/Main06.java
+++ src/main/java/com/example/javaexercises5/dateapi/Task06/Main06.java
@@ -1,14 +1,14 @@
 package com.example.javaexercises5.dateapi.Task06;
 
-import java.time.Duration;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 public class Main06 {
     public static void main(String[] args) {
-        LocalDateTime dateTime = LocalDateTime.of(2022, 8, 27, 18, 0);
-        LocalDateTime dateTime2 = LocalDateTime.of(2022, 8, 28, 9, 0);
-        LocalDateTime dateTime3 = LocalDateTime.of(dateTime.toLocalDate().plusDays(1), LocalTime.of(0,0));
-        System.out.println(Duration.between(dateTime, dateTime3));
+        ZonedDateTime zonedDateTime = ZonedDateTime.now(ZoneId.systemDefault());
+        ZonedDateTime zonedDateTime2 = zonedDateTime.withZoneSameInstant(ZoneId.of("UTC-2"));
+        System.out.println(zonedDateTime);
+        System.out.println(zonedDateTime2);
+        System.out.println(zonedDateTime.isEqual(zonedDateTime2));
     }
 }
